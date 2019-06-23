d99d54620bfceeb4a650abcc43eeb87e046955b9
Jefferson Clark <jefferson@clarkitect.net>
2019-06-22 20:16:32 -0500

    adds more map resources
    * temp tileset for wild
    * temp map for world
    * totally new tileset images

---
+++ game/art/tile/wild.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="wild" tilewidth="16" tileheight="16" spacing="1" tilecount="1537" columns="53">
+ <image source="../../../../spire-road/WebContent/assets/images/tiles/realm.png" width="912" height="496"/>
+</tileset>
