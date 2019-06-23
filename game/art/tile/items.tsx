792934946c6271941cd7d6c35f2109e341e3829f
Jefferson Clark <jefferson@clarkitect.net>
2019-06-22 22:25:25 -0500

    adds tilesets and basic realm map

---
+++ game/art/tile/items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="items" tilewidth="16" tileheight="16" tilecount="660" columns="33">
+ <image source="../pics/objects.png" width="528" height="320"/>
+</tileset>
