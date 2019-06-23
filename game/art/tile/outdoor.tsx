8e44def582da95f4863ebb12e7240762df5c87d2
Jefferson Clark <jefferson@clarkitect.net>
2019-06-22 20:20:36 -0500

    removes old tiles, images, maps

--- game/art/tile/outdoor.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="outdoor" tilewidth="16" tileheight="16" spacing="1" tilecount="1920" columns="57">
- <grid orientation="orthogonal" width="8" height="8"/>
- <image source="roguelikeSheet_transparent.png" width="968" height="526"/>
-</tileset>
